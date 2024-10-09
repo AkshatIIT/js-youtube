f5a322d972eb9e7fe872c7fc4722059a1976082b
Akshat <108949815+AkshatIIT@users.noreply.github.com>
2024-10-09 08:31:59 +0000

    operation

--- 01-basic/03_conversionOperation.js
+++ 01-basic/03_conversionOperation.js
@@ -12,7 +12,17 @@ console.log(valueIn)// NaN is coming because we have score "33abc" which is conv
 // .. to boolean
 // 1=> true, 0=> false
 //""=> "hitesh"=> true
-//
+/////*******Operation*********** */
+let value=3;
+let negvalue=-value
+console.log(negvalue)
+console.log("1"-2)
+console.log(null>0)// false
+console.log(null==0) //false
+console.log(null>=0)// true
+// this happen because equality and comparisons operator works differently. Comparison convert null into number, treating it as 0.
+console.log('2'===2) // this is strict check that mean it will also check the data type of it. 
+
 
 
 
