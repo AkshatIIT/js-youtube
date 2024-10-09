cffc857539b7daf0d5cc8e03b5d33c92a0925348
Akshat <108949815+AkshatIIT@users.noreply.github.com>
2024-10-09 07:33:10 +0000

    Conversion

---
+++ 01-basic/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score= "33abc"
+let score_01=true 
+console.log(typeof score)
+let valueIn=Number(score)
+console.log(typeof valueIn)
+console.log(valueIn)// NaN is coming because we have score "33abc" which is converted in number but NaN.
+// conversion
+// ... to number
+// "33"=> 33
+// "33abc"=> NaN
+// true=> 1; false=>0
+// .. to boolean
+// 1=> true, 0=> false
+//""=> "hitesh"=> true
+//
+
+
+
