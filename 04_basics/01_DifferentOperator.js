cf0e8650b7e12a522e6ab2deed48d82e3e0b40d8
Akshat <108949815+AkshatIIT@users.noreply.github.com>
2024-10-14 12:09:44 +0000

    operators

---
+++ 04_basics/01_DifferentOperator.js
@@ -0,0 +1,10 @@
+//Nullish Coalescing Operation(??): null and undefined. 
+//in this operator, if we got null value from somewhere then we have one more value that can be assigned to a variable. Note: First value get assigned to 
+let val1;
+val1=5 ?? 10 //it returns 5
+val1=null ?? 10 //it returns 10
+//Terniary Operator
+//condition ?true:false. Eg as follows
+const iceTeaPrice=100;
+iceTeaPrice<=80?console.log("Lessthan80"):console.log(">=80")
+
